9f1fa55917a75b07a07841aba8eeb98f98c9511d
LeoCastroo <leodcastro97@gmail.com>
2023-01-22 14:54:46 -0300

    feat: Criando os componentes que permitam a inserção de novos hábitos e a verificação da sua quantidade

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,22 @@
+interface ProgressBar {
+    progress:number
+}
+
+export function ProgressBar(props: ProgressBar) {
+    
+    const progressStyle = {
+        width: `${props.progress}%`
+    }
+
+    return (
+        <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+            <div
+                role="progressbar"
+                aria-label="Progresso de hábitos completados nesse dia"
+                aria-valuenow={props.progress}
+                className="h-3 rounded-xl bg-violet-600"
+                style={progressStyle}
+            />
+        </div>
+    )
+}
--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -23,14 +23,18 @@ export function SummaryTable() {
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {
                     summaryDates.map(date => {
-                        return <HabitDay key={date.toString()} />
+                        return <HabitDay
+                            key={date.toString()}
+                            amount={5}
+                            completed={Math.round(Math.random() * 5)} 
+                         />
                     })
                 }
                 {amountOfDaystofill > 0 && Array.from({
                     length: amountOfDaystofill
                 }).map((_, i) => {
                     return (
-                        <div key={i} className="w-10 h-10 bg-zinc-900 border border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"/>
+                        <div key={i} className="w-10 h-10 bg-zinc-900 border border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
                     )
                 })}
             </div>
