d0a4d6403890301b92accba160ae2894105f5138
LeoCastroo <leodcastro97@gmail.com>
2023-01-23 16:49:23 -0300

    feat: Novas alterações de layout e conectando a aplicação a API

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,3 +1,7 @@
+import dayjs from "dayjs";
+import { UserSquare } from "phosphor-react";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-begninning"
 import { HabitDay } from "./HabitDay"
 
@@ -8,7 +12,23 @@ const summaryDates = generateDatesFromYearBeginning()
 const minimumSumaryDatesSize = 18 * 7; //18 weeks
 const amountOfDaystofill = minimumSumaryDatesSize - summaryDates.length;
 
+type Summary = {
+    id: string;
+    date: string;
+    amount: number;
+    completed: number;
+}[]
+
 export function SummaryTable() {
+
+    const [summary, setSummary] = useState<Summary>([])
+    
+    useEffect(() => {
+        api.get('summary').then(response => {
+           setSummary(response.data)
+        })
+    }, [])
+    
     return (
         <div className="w-full flex">
             <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -23,10 +43,16 @@ export function SummaryTable() {
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {
                     summaryDates.map(date => {
+
+                        const dayInSummary = summary.find(day => {
+                            return dayjs(date).isSame(day.date, 'day');
+                        })
+
                         return <HabitDay
                             key={date.toString()}
-                            amount={5}
-                            completed={Math.round(Math.random() * 5)} 
+                            date={date}
+                            amount={dayInSummary?.amount}
+                            completed={dayInSummary?.completed} 
                          />
                     })
                 }
